0861acecce7e308a7e6580a735f4c01bb35d8784
Darío Rivera <fermius.us@gmail.com>
2020-12-05 15:54:24 -0500

    refactor: improve indentation

--- tests/Mocks/HttpInfo.php
+++ tests/Mocks/HttpInfo.php
@@ -11,20 +11,20 @@ class HttpInfo extends BaseMock
         return $this->response(
             200,
             [
-            'method' => $request->getMethod(),
-            'body' => $request->getBody()->getContents(),
-            'uri' =>
-                $request->getUri()->getScheme() . '://' .
-                $request->getUri()->getHost() .
-                $request->getUri()->getPath(),
-            'uriFragment' => [
-                'schema' => $request->getUri()->getScheme(),
-                'port' => $request->getUri()->getPort(),
-                'host' => $request->getUri()->getHost(),
-                'path' => $request->getUri()->getPath(),
-                'query' => $request->getUri()->getQuery(),
-            ],
-            'headers' => $this->parseHeaders($request->getHeaders()),
+                'method' => $request->getMethod(),
+                'body' => $request->getBody()->getContents(),
+                'uri' =>
+                    $request->getUri()->getScheme() . '://' .
+                    $request->getUri()->getHost() .
+                    $request->getUri()->getPath(),
+                'uriFragment' => [
+                    'schema' => $request->getUri()->getScheme(),
+                    'port' => $request->getUri()->getPort(),
+                    'host' => $request->getUri()->getHost(),
+                    'path' => $request->getUri()->getPath(),
+                    'query' => $request->getUri()->getQuery(),
+                ],
+                'headers' => $this->parseHeaders($request->getHeaders()),
             ]
         );
     }
