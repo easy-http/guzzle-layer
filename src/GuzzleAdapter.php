dcf150dff8042643ec03a4f6269ae046075d61c1
Darío Rivera <fermius.us@gmail.com>
2022-04-09 10:32:08 -0500

    feat: update layer-contracts to v1.0.0

--- src/GuzzleAdapter.php
+++ src/GuzzleAdapter.php
@@ -6,7 +6,9 @@ use EasyHttp\LayerContracts\Contracts\HttpClientAdapter;
 use EasyHttp\LayerContracts\Contracts\HttpClientRequest;
 use EasyHttp\LayerContracts\Contracts\HttpClientResponse;
 use EasyHttp\LayerContracts\Exceptions\HttpClientException;
+use EasyHttp\LayerContracts\Exceptions\HttpConnectionException;
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\ConnectException;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Exception\TransferException;
 
@@ -22,17 +24,53 @@ class GuzzleAdapter implements HttpClientAdapter
     public function request(HttpClientRequest $request): HttpClientResponse
     {
         try {
-            $response = $this->client->request($request->getMethod(), $request->getUri(), $request->options());
+            $response = $this->client->request(
+                $request->getMethod(),
+                $request->getUri(),
+                $this->buildOptions($request)
+            );
+        } catch (ConnectException $exception) {
+            throw new HttpConnectionException($exception->getMessage(), (int) $exception->getCode(), $exception);
         } catch (RequestException $exception) {
             if (! $exception->hasResponse()) {
-                throw HttpClientException::fromThrowable($exception);
+                throw new HttpClientException($exception->getMessage(), (int) $exception->getCode(), $exception);
             }
 
             $response = $exception->getResponse();
         } catch (TransferException $exception) {
-            throw HttpClientException::fromThrowable($exception);
+            throw new HttpClientException($exception->getMessage(), (int) $exception->getCode(), $exception);
         }
 
         return new GuzzleResponse($response);
+    }
+
+    private function buildOptions(HttpClientRequest $request): array
+    {
+        $options = [
+            'timeout' => $request->getTimeout(),
+            'verify' => $request->isSSL()
+        ];
+
+        if ($request->hasHeaders()) {
+            $options['headers'] = $request->getHeaders();
+        }
+
+        if ($request->hasJson()) {
+            $options['json'] = $request->getJson();
+        }
+
+        if ($request->hasQuery()) {
+            $options['query'] = $request->getQuery();
+        }
+
+        if ($request->hasSecurityContext() && $request->getSecurityContext()->hasCertificate()) {
+            $options['cert'] = $request->getSecurityContext()->getCertificate();
+        }
+
+        if (count($request->getBasicAuth())) {
+            $options['auth'] = $request->getBasicAuth();
+        }
+
+        return $options;
     }
 }
